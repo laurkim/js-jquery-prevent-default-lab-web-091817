47dc4998378dd42ee6837c7588c5181e783b6e09
laurkim <laurakeem@gmail.com>
2017-11-04 16:22:31 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
 // define functions here
+
+function submitForm() {
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    const item = $('#item').val();
+    const list = $('ol');
+    list.append(`<li>${item}</li>`);
+  })
+}
